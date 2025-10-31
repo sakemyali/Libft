a1e382f2416a24691624fedd223dfcd5c164b69e
Moulay Ali Sakurai El Idrissi <mosakura@c2r2s9.42tokyo.jp>
2025-10-31 18:52:24 +0900

    changed a mistake in the logic for lstaddfront

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mosakura <mosakura@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 18:50:51 by mosakura          #+#    #+#             */
-/*   Updated: 2025/10/30 21:13:28 by mosakura         ###   ########.fr       */
+/*   Updated: 2025/10/31 18:09:47 by mosakura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	if (!lst || !new)
 		return ;
-	ft_lstlast(new)->next = *lst;
+	new->next = *lst;
 	*lst = new;
 }
